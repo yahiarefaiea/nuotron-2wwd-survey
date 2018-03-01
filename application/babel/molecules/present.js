65eb4adb33f3b01b865e99a5d6fe8e5ac8d45337
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-03-01 15:37:31 +0200

    present function working just fine

--- application/babel/molecules/present.js
+++ application/babel/molecules/present.js
@@ -1,3 +1,23 @@
 function runPresent() {
   $('.wrapper').addClass('present')
+  timeForEach = 1500
+
+  function next() {
+    item = '#present li'
+    current = $(item+'.current')
+
+    // if(!$(item).hasClass('current')) {
+    //   $(item+':first-child').addClass('current')
+    // } else {
+    //   current.removeAttr('class').next().addClass('current')
+    //
+    //   if(current.next().is('#p6')) {
+    //     $('.wrapper').removeClass('present').addClass('survey start')
+    //     clearInterval(interval)
+    //   }
+    // }
+  }
+
+  interval = setInterval(next(), timeForEach)
+
 }
