b959e3b68b3d44690679fb270821ffd983326a52
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-03-21 12:58:14 +0200

    finalize range functionality and styles

--- gulpfile.js
+++ gulpfile.js
@@ -87,6 +87,7 @@ var babelSrc = [
   root+'/babel/lib/jquery-2.2.4.js',
   root+'/babel/lib/jpreloader.js',
   root+'/babel/lib/tippy.all.min.js',
+  root+'/babel/molecules/range.js',
   root+'/babel/molecules/spans.js',
   root+'/babel/molecules/nucubuc.js',
   root+'/babel/organisms/present.js',
