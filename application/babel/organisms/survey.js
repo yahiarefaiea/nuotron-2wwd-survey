73f281b88c8ddf7edf5b71b940d70d6aa1dcccb1
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-03-26 14:26:46 +0200

    fix few other issues in the prepare() method

--- application/babel/organisms/survey.js
+++ application/babel/organisms/survey.js
@@ -155,6 +155,13 @@ var survey = {
       var fallback = 'Passed empty..'
       if(answer == '' || answer === undefined) answer = fallback
 
+      //  HANDLING WHAT HAPPENS IF DATA NAME WAS FEELING
+      if(question.attr('data-name') == 'feeling') {
+        list = question.find('input[type=range]').siblings('ul')
+        item = list.find('li:nth-child('+answer+')')
+        answer = item.text()
+      }
+
       //  HANDLING WHAT HAPPENS IF DATA NAME WAS CONTACT
       if(question.attr('data-name') == 'contact') {
         answer = []
@@ -205,15 +212,33 @@ var survey = {
 
       //  03. Create the Ajax request body then => {
 
-        //  3.1. Create status in the response with the
-        //       values of 'error' or 'success'.
-        //       Try to switch between the status values
-        //       to have an idea about what should
-        //       exactly happens
+        //  03.0. What's going to happen in the server side?
+
+          //  03.0.1 You should create 2 variables that
+          //         contains the email body you'll be sending
+          //         plus, injecting the values from the
+          //         preparedData object
+          //         var feedbackReceived = 'Email sent for me'
+          //         var feedbackResponse = 'Email sent to ${data.contact.FullName}' (which is for the user)
+
+          //  03.0.2 Handle the request and return a status
+          //         to the ui, read `03.1 + 03.2` for more..
+
+          //  03.0.3 Send two emails..
+          //         One for me via:
+          //         me@yahiarefaiea.com
+          //         And one for the user (If he passed an email)
+          //         me@yahiarefaiea.com
+
+        //  03.1. Create status in the response with the
+        //        values of 'error' or 'success'.
+        //        Try to switch between the status values
+        //        to have an idea about what should
+        //        exactly happens
         var status = 'error'
 
-        //  3.2. Call this method when you're all
-        //       done, and pass in the status
+        //  03.2. Call this method when you're all
+        //        done, and pass in the status
         this.callback(status)
 
       //  }
